c1524ae8057b3edf7a4d51709f75d58dd7d5da81
willowsenator <willowsenator@gmail.com>
2022-03-07 00:31:46 +0100

    add connect smartContract with web3

---
+++ src/web3.js
@@ -0,0 +1,52 @@
+import abi from "abi/abi.json" assert {type: "json"};
+
+// SC: 0x6fb89b812c757b87e4dcc638d284e8735c9229bd
+
+const blockchain = new Promise((res, err) =>{
+    // Metamask is not available
+    if(typeof window.ethereum == "undefined"){
+        err("You should install Metamask");
+    }
+
+    // Instance Web3
+    let web3 = new Web3(window.ethereum);
+    let contract = new web3.eth.Contract(abi, 0x6fb89b812c757b87e4dcc638d284e8735c9229bd);
+
+    // Get my Metamask address
+    web3.eth.getAccounts().then((accounts)=>{
+        console.log("-> My account is: ", accounts[0]);
+    } );
+
+
+    // Get current supply of NFT Token
+    web3.eth.getAccounts().then((accounts)=>{
+        contract.methods.getTotalSupply().call({from: accounts[0]}).then((supply) =>{
+            console.log("-> Current supply of NTF Token: ", supply);
+        })
+    } );
+
+
+    // Get MaxSupply of NFT Token
+    web3.eth.getAccounts().then((accounts)=>{
+        contract.methods.maxSupply().call({from: accounts[0]}).then((maxSupply) =>{
+            console.log("-> Max supply of NTF Token: ", maxSupply);
+        })
+    } );
+
+    // Get your own buildings in the metaverse
+    web3.eth.getAccounts().then((accounts)=>{
+        contract.methods.getOwnerBuildings().call({from: accounts[0]}).then((buildings) =>{
+            console.log("-> Your buildings: ", buildings);
+        })
+    } );
+
+
+    // Get all the buildings in the metaverse
+    web3.eth.getAccounts().then((accounts)=>{
+        contract.methods.getTotalSupply().call({from: accounts[0]}).then((supply) =>{
+           contract.methods.getBuildings().call({from: accounts[0]}).then((data)=>{
+               res({supply: supply, building: data});
+           });
+        });
+    } );
+})
